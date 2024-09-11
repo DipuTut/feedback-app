cdcadae59b2f40822ee7d2814604db53add8208f
DipuTut <mddipu@gmail.com>
2024-09-11 11:54:19 +0200

    Feedback-App clone here

---
+++ scripts/userdata.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir /home/ec2-user/feedback-app
+cd /home/ec2-user/feedback-app
+#Install Docker
+yum update -y
+yum install -y docker
+
+#Start Docker
+service docker start
+systemctl enable docker
+
+#Install Docker-Compose
+curl -L "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+#Download Docker-Compose App Config
+mkdir /home/ec2-user/feedback-app
+cd /home/ec2-user/feedback-app
+wget https://raw.githubusercontent.com/atamankina/feedback-app/main/docker-compose.yml
+
+#Start the app
+sudo docker-compose up -d
